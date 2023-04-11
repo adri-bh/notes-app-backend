aedfdfa98a3ce8c839943610890a7c294c1f5e88
adri <adribh23@gmail.com>
2023-04-11 18:40:44 +0800

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,41 @@
+const {addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler } = require('./handler.js');
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: addNoteHandler,
+        // options: { // response.header('Access-Control-Allow-Origin', '*');
+        //     cors: {
+        //       origin: ['*'],
+        //     },
+        //   },
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNoteByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editNoteByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: deleteNoteByIdHandler,
+    },
+    
+    
+    
+];
+
+
+
+module.exports = routes;
