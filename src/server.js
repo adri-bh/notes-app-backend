229a8def1c170fbcaa77b4e873ff31ec50becb52
adri <adribh23@gmail.com>
2023-04-11 22:18:17 +0800

    salah penulisan proccess

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 const init = async ()=>{
     const server = Hapi.server({
         port : 5000,
-        host : procces.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+        host : proccess.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         routes: { 
             cors: {
               origin: ['*'],
